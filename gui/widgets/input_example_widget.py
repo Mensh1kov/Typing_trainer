4791e077b77e554641bd720b70296e0cd0de37de
Mensh1kov <www.sasha.menshikov018@mail.ru>
2023-04-18 10:40:47 +0500

    Добавлена проверка на соответствие примера и введенного текста

--- gui/widgets/input_example_widget.py
+++ gui/widgets/input_example_widget.py
@@ -9,15 +9,31 @@ class InputExampleWidget:
         self.input_and_example_widget.setObjectName("input_and_example_widget")
         self.input = QtWidgets.QLineEdit(self.input_and_example_widget)
         self.input.setGeometry(QtCore.QRect(0, 0, 700, 30))
-        self.font = QtGui.QFont()
-        self.font.setPointSize(12)
-        self.input.setFont(self.font)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.input.setFont(font)
         self.input.setObjectName("input")
+        self.input.textChanged.connect(lambda: self.on_text_changed())
         self.example = QtWidgets.QLabel(self.input_and_example_widget)
         self.example.setGeometry(QtCore.QRect(3, 35, 694, 60))
-        self.example.setFont(self.font)
+        self.example.setFont(font)
         self.example.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
         self.example.setObjectName("example")
+        self.example.setText('Мама мыла раму')
+        self.mistakes = 0
 
-    def set_example(self, example: str):
-        self.example.setText(example)
+    def on_text_changed(self):
+        text = self.input.text()
+        target_text = self.example.text()
+        current_position = self.input.cursorPosition()
+
+        if text != target_text[:current_position]:
+            self.input.setStyleSheet("background-color: red;")
+            self.mistakes += 1
+            print(self.mistakes)
+        else:
+            self.input.setStyleSheet("background-color: white;")
+
+        if text == target_text:
+            # self.complete()
+            print('молодец')
